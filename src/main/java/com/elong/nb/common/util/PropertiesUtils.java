8de7011ae44fdfc8cdb4708cc61e6c372e2247f5
王文集 <wenjiwork@163.com>
2016-05-13 15:20:22 +0800

    生成AOS要求的配置文件

--- src/main/java/com/elong/nb/common/util/PropertiesUtils.java
+++ src/main/java/com/elong/nb/common/util/PropertiesUtils.java
@@ -1,10 +1,19 @@
 package com.elong.nb.common.util;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.Properties;
+
 public class PropertiesUtils {
-	public static Properties loadProperties(String contentRoot,String filePath){
+	
+	public static Properties loadProperties(String contentRoot, String filePath) {
 		String fileDir = contentRoot + filePath;
 		Properties props = new Properties();
 		FileInputStream istream = null;
@@ -12,10 +21,10 @@ public class PropertiesUtils {
 			istream = new FileInputStream(fileDir);
 			props.load(istream);
 		} catch (IOException e) {
-			System.out.println("[读取Properties文件异常]"+"IO异常："+fileDir);
+			System.out.println("[读取Properties文件异常]" + "IO异常：" + fileDir);
 			e.printStackTrace();
-		}finally{
-			if(istream != null){
+		} finally {
+			if (istream != null) {
 				try {
 					istream.close();
 					istream = null;
@@ -26,13 +35,64 @@ public class PropertiesUtils {
 		}
 		return props;
 	}
+
 	private static String getClassesPath() {
 		return PropertiesUtils.class.getResource("/").getPath();
 	}
-	public static Properties loadProperties(String fileName){
-		return loadProperties(getClassesPath(),fileName);
+
+	public static Properties loadProperties(String fileName) {
+		return loadProperties(getClassesPath(), fileName);
 	}
-	public static void main(String[] args) {
+
+	/**
+	 * 
+	 * 生成AOS要求的value为$$key形式的properties文件
+	 *
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public static void generateAOSPropertiesFile() throws FileNotFoundException, IOException {
+		// 遍历文件夹路径
+		String filePath = "/Users/user/Desktop/properties";
+		// 获取所有文件名
+		List<String> fileList = getFiles(filePath);
+		// 遍历所有properties文件
+		for (String fileName : fileList) {
+			Properties properties = new Properties();
+			properties.load(new FileInputStream(fileName));
+			Enumeration enumeration = properties.propertyNames();
+			while (enumeration.hasMoreElements()) {
+				String key = (String) enumeration.nextElement();
+				properties.setProperty(key, "$$" + key);
+			}
+			// 输出
+			properties.store(new FileOutputStream(fileName), "修改时间：" + new Date());
+		}
+	}
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		System.out.println(PropertiesUtils.class.getResource("/").getPath());
+		generateAOSPropertiesFile();
+	}
+
+	/**
+	 * 
+	 * 递归查找根目录下所有文件
+	 *
+	 * @param path
+	 * @return
+	 */
+	private static List<String> getFiles(String path) {
+		List<String> fileList = new ArrayList<String>();
+		File root = new File(path);
+		File[] files = root.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				getFiles(file.getAbsolutePath());
+			} else {
+				fileList.add(file.getAbsolutePath());
+			}
+		}
+		return fileList;
 	}
 }
