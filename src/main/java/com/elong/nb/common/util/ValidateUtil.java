993fbdec82f39c8c542fcf12072e04635eb78bef
房磊 <lei.fang@corp.elong.com>
2016-07-05 16:21:23 +0800

    local能为空

--- src/main/java/com/elong/nb/common/util/ValidateUtil.java
+++ src/main/java/com/elong/nb/common/util/ValidateUtil.java
@@ -7,7 +7,6 @@ import com.elong.nb.common.model.RestRequest;
 public class ValidateUtil {
 
 	private static final String Version = "Version不能为空";
-	private static final String Local = "Local不能为空";
 	private static final String ProxyAccount = "企业账户信息不能为空";
 	private static final String Request = "H000016|Data参数必须填写";
 	private static final String Guid = "H000017|GUID参数必须填写";
@@ -16,8 +15,6 @@ public class ValidateUtil {
 		StringBuffer sb = new StringBuffer();
 		if (req.getVersion() == null || req.getVersion() <= 0d)
 			sb.append(Version);
-		if (req.getLocal() == null)
-			sb.append(Local);
 		if (req.getRequest() == null)
 			sb.append(Request);
 		if (req.getProxyInfo() == null)
