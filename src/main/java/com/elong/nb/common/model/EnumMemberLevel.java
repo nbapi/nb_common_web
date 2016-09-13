f4872190d8da64f5ea8d3c1065e3256998a93230
yingquan.li <yingquan.li@corp.elong.com>
2016-09-13 12:28:15 +0800

    会员等级添加VIP和SVIP

--- src/main/java/com/elong/nb/common/model/EnumMemberLevel.java
+++ src/main/java/com/elong/nb/common/model/EnumMemberLevel.java
@@ -8,21 +8,16 @@ public enum EnumMemberLevel
 {
 	/** 
 	 普通会员
-	 
 	*/
-	Normal(1);
+	Normal(1),
 	/** 
 	 VIP会员
-	 
-	//VIP = 2,
 	*/
-
+	VIP(2),
 	/** 
 	 龙萃会员
-	 
-	//SVIP = 4,
 	*/
-
+	SVIP(4);
 	private int intValue;
 	private static java.util.HashMap<Integer, EnumMemberLevel> mappings;
 	private synchronized static java.util.HashMap<Integer, EnumMemberLevel> getMappings()
