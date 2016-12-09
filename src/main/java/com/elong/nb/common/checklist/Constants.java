10aad1ba080be4b09f8df8f10dad1f11743b3aaa
房磊 <lei.fang@corp.elong.com>
2016-12-09 18:52:21 +0800

    添加checklist3.0的常量； 按百分比判断的记录redis类监控的工具类

---
+++ src/main/java/com/elong/nb/common/checklist/Constants.java
@@ -0,0 +1,8 @@
+package com.elong.nb.common.checklist;
+
+public class Constants {
+	
+	public static final String ELONG_REQUEST_REQUESTGUID = "elongRequestGuid";
+	
+	public static final String ELONG_REQUEST_JSON = "elongRequestJson";
+}
