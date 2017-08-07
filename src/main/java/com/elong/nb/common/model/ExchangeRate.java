23c466b9c249f512dd22f8100b7282b0ba83f983
苏红涛 <hongtao.su@corp.elong.com>
2017-08-07 18:52:48 +0800

    获取汇率信息Agent

---
+++ src/main/java/com/elong/nb/common/model/ExchangeRate.java
@@ -0,0 +1,20 @@
+package com.elong.nb.common.model;
+
+
+public class ExchangeRate {
+	private String	CurrencyCode;
+	private double Rate;
+	public String getCurrencyCode() {
+		return CurrencyCode;
+	}
+	public void setCurrencyCode(String currencyCode) {
+		CurrencyCode = currencyCode;
+	}
+	public double getRate() {
+		return Rate;
+	}
+	public void setRate(double rate) {
+		Rate = rate;
+	}
+	
+}
