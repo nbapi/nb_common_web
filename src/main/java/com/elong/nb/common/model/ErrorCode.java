3177139cc8cffd445f0d928e0ed82981265dcfbc
zhangyang.zhu <zhangyang.zhu@corp.elong.com>
2017-12-22 14:14:37 +0800

    取消接口错误码

--- src/main/java/com/elong/nb/common/model/ErrorCode.java
+++ src/main/java/com/elong/nb/common/model/ErrorCode.java
@@ -239,7 +239,7 @@ public interface ErrorCode {
 	
 	public static final String Order_HourRoomValidate="H001138|小时房最晚到店时间不能晚于或等于{0}";
 	
-	public static final String Order_CancelPenayNotEnough="H001139|取消罚金不足";
+	public static final String Order_CancelPenayNotEnough="H001139|取消接口罚金不足,应为:{0}";
 	// endregion
 
 	// region 搜索 H002001
